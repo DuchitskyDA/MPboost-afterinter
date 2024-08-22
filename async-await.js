030b707bb4c9318943df0b49fdf2343548187bb8
DuchitskyDA <danila.f1n1@yandex.ru>
2024-08-22 16:37:48 +0300

    Выполненные задачи

--- async-await.js
+++ async-await.js
@@ -6,15 +6,15 @@
 //     })
 // };
 
- const sendRequest = async id => {
-    try {
-        const { item } = await api.getItem(id);
-        const res = await api.getItemData(item.uid);
-        // дальше какая-то логика
-    } catch (e) {
-        throw new Error(e);
-    } finally {
-        // для каких то действий при любом исходе
-        // например, чтобы убрать прелоадер
-    }
-}
+const sendRequest = async (id) => {
+  try {
+    const { item } = await api.getItem(id);
+    const res = await api.getItemData(item.uid);
+    // дальше какая-то логика
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // для каких то действий при любом исходе
+    // например, чтобы убрать прелоадер
+  }
+};
